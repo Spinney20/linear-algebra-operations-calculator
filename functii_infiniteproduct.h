043a70cbf2c7a4281ba6121dee85e800aa81297f
Dobre Andrei-Teodor <116495955+Spinney20@users.noreply.github.com>
2023-11-27 22:15:15 +0200

    Added functii_infiniteproduct.h

---
+++ functii_infiniteproduct.h
@@ -0,0 +1,8 @@
+//Dobre Andrei-Teodor 314CA
+#pragma once
+#define ull unsigned long long
+#define ll long long
+#define db double
+#include <stdio.h>
+
+void afisare(int oka, int okb, ll ps, ull a_max, ull b_max, db n_a, db n_b);
